75fa34a361f6167ce8620f90b0afe34125dd8f1f
Tuan Vu <tuanvcw@gmail.com>
2015-07-17 08:20:05 +0700

    commit blogger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -6,4 +6,37 @@ class ArticlesController < ApplicationController
   def show
     @articles = Article.find(params[:id])
   end
+
+  def new
+    @articles = Article.new
+  end
+
+  def create
+	@articles = Article.new(article_params)
+	@articles.save
+	redirect_to @articles
+  end
+
+  def destroy
+    @articles = Article.find(params[:id])
+    @articles.destroy
+    redirect_to articles_path
+  end
+
+  def edit
+	@articles = Article.find(params[:id])
+  end
+
+  def update
+	@articles = Article.find(params[:id])
+	if @articles.update(article_params)
+	  redirect_to @articles
+	else
+	  render 'edit'
+	end
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :body)
+  end
 end
